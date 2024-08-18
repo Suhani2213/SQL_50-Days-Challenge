ffae3e4ac4c4bd1fd6e389fec73e89b8c057dcc7
Suhani2213 <suhani2213@gmail.com>
2024-08-18 21:24:48 +0530

    Update Day 13 - SQL Challenge.sql

--- Day 13 - SQL Challenge.sql
+++ Day 13 - SQL Challenge.sql
@@ -7,6 +7,7 @@ teamID varchar(2),
 memberID varchar(10),
 Criteria1 varchar(1),
 Criteria2 varchar(1)
+  
 );
 insert into Ameriprise_LLC values 
 ('T1','T1_mbr1','Y','Y'),
@@ -25,6 +26,7 @@ insert into Ameriprise_LLC values
 ('T3','T3_mbr4','N','Y'),
 ('T3','T3_mbr5','Y','N');
 
+
 -- If both Criteria1 and Criteri2 are 'Y' for at least 2 team members within the same team, then entire team is eligible for qualifing else team is not eligible for qualification.
 -- Show the members who actually qualified and did not qualify the program.
 
@@ -32,13 +34,14 @@ SELECT * FROM Ameriprise_LLC;
 
 
 WITH cte as (
-SELECT teamID, COUNT(teamID) qualifying_team
+SELECT teamID, 
+       COUNT(teamID) qualifying_team
 FROM Ameriprise_LLC
 WHERE Criteria1 = 'Y' AND Criteria2 = 'Y' 
 GROUP BY teamID
 HAVING COUNT(teamID) >= 2
 )
 SELECT a.*,
-CASE WHEN a.Criteria1 = 'Y' AND a.Criteria2 = 'Y' AND qualifying_team >= 2 THEN "YES! Qualified" ELSE "Better Luck Next Time" END Result
+      CASE WHEN a.Criteria1 = 'Y' AND a.Criteria2 = 'Y' AND qualifying_team >= 2 THEN "YES! Qualified" ELSE "Better Luck Next Time" END Result
 FROM Ameriprise_LLC a
 LEFT JOIN cte c ON c.teamID = a.teamID;
